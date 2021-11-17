d813eb2566e744192244ff881d05692ccda0fbff
nikhilq <84630781+nikhilq@users.noreply.github.com>
2021-11-17 15:20:26 +0530

    Update chatbot.py

--- NaoRobot/modules/chatbot.py
+++ NaoRobot/modules/chatbot.py
@@ -56,9 +56,9 @@ async def hmm(_, message):
         lel = await edit_or_reply(message, "`Processing...`")
         lol = add_chat(int(message.chat.id))
         if not lol:
-            await lel.edit("Sayonara AI Already Activated In This Chat")
+            await lel.edit("Nikhil AI Already Activated In This Chat")
             return
-        await lel.edit(f"Sayonara AI Successfully Added For Users In The Chat {message.chat.id}")
+        await lel.edit(f"Nikhil AI Successfully Added For Users In The Chat {message.chat.id}")
 
     elif status == "OFF" or status == "off" or status == "Off":
         lel = await edit_or_reply(message, "`Processing...`")

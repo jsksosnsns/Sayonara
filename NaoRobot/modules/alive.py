7bc7ab02667d7de082bd20af00224cb5a6f494f2
nikhilq <84630781+nikhilq@users.noreply.github.com>
2021-11-17 15:21:56 +0530

    Update alive.py

--- NaoRobot/modules/alive.py
+++ NaoRobot/modules/alive.py
@@ -7,16 +7,16 @@ from pyrogram import __version__ as pyrover
 from NaoRobot.events import register as MEMEK
 from NaoRobot import telethn as tbot
 
-PHOTO = "https://telegra.ph/file/ca39a509359ea8528a18a.jpg"
+PHOTO = "https://telegra.ph/file/daad4d89b74ee78d95c46.jpg"
 
 @MEMEK(pattern=("/alive"))
 async def awake(event):
   tai = event.sender.first_name
-  NAO = f"**Holla, I'm Sayonara Robot!** \n\n"
+  NAO = f"**Holla, I'm Nikhil Robot!** \n\n"
   NAO += "🔴 **I'm Working Properly** \n\n"
-  NAO += "🔴 **My Master : [Sayonara](https://t.me/Sayonara_nih)** \n\n"
+  NAO += "🔴 **My Master : [Sayonara](https://t.me/dost_hai_sab)** \n\n"
   NAO += f"🔴 **Telethon Version : {tlhver}** \n\n"
   NAO += f"🔴 **Pyrogram Version : {pyrover}** \n\n"
   NAO += "**Thanks For Adding Me Here ❤️**"
-  BUTTON = [[Button.url("ʜᴇʟᴘ", "https://t.me/Sayonararobot_bot?start=help"), Button.url("sᴜᴘᴘᴏʀᴛ", "https://t.me/NaraXmusic")]]
+  BUTTON = [[Button.url("ʜᴇʟᴘ", "https://t.me/Sayonararobot_bot?start=help"), Button.url("sᴜᴘᴘᴏʀᴛ", "https://t.me/dost_hai_sab")]]
   await tbot.send_file(event.chat_id, PHOTO, caption=NAO,  buttons=BUTTON)

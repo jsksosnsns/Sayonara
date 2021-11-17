40cebb8389b5725e8aa6cf5436fb5d7e2f824756
nikhilq <84630781+nikhilq@users.noreply.github.com>
2021-11-17 15:13:05 +0530

    Update chatbot.py

--- NaoRobot/modules/chatbot.py
+++ NaoRobot/modules/chatbot.py
@@ -64,9 +64,9 @@ async def hmm(_, message):
         lel = await edit_or_reply(message, "`Processing...`")
         Escobar = remove_chat(int(message.chat.id))
         if not Escobar:
-            await lel.edit("Sayonara AI Was Not Activated In This Chat")
+            await lel.edit("Nikhil Was Not Activated In This Chat")
             return
-        await lel.edit(f"Sayonara AI Successfully Deactivated For Users In The Chat {message.chat.id}")
+        await lel.edit(f"Nikhil Successfully Deactivated For Users In The Chat {message.chat.id}")
 
     elif status == "EN" or status == "en" or status == "english":
         if not chat_id in en_chats:
